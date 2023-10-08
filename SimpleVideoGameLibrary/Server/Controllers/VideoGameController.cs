77925b57b1fec66ebcc2c05490d9b053ea1564e2
Tharaka Madushan <mtharaka37@gmail.com>
2023-10-08 21:53:52 +0530

    Add project files.

---
+++ SimpleVideoGameLibrary/Server/Controllers/VideoGameController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleVideoGameLibrary.Server.Data;
+using SimpleVideoGameLibrary.Shared;
+
+namespace SimpleVideoGameLibrary.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public VideoGameController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult<List<VideoGame>>> GetAllVideoGames()
+        {
+
+            #region Dummy Data Commented By TharakaMadushan-20231008
+            //following data consider as dummy data
+            //var list = new List<VideoGame>
+            //{
+            //    new VideoGame{Id = 1, Title = "Super Mario", Publisher = "Disney", ReleaseYear = 1999},
+            //    new VideoGame{Id = 2, Title = "NFS", Publisher = "EA Games", ReleaseYear = 2000},
+            //}; 
+            #endregion
+
+            var list = await _context.VideoGames.ToListAsync();
+            return Ok(list);
+        }
+
+
+    }
+
+}
---
+++ SimpleVideoGameLibrary/Shared/VideoGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleVideoGameLibrary.Shared
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public int ReleaseYear  { get; set; }
+    }
+}
